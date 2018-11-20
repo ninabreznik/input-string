047e458bbcb5083e03adde6e9aef2cea058eeb03
ninabreznik <ninabreznik@gmail.com>
2018-11-20 05:06:20 +0100

    Add validation cb

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 var bel = require('bel')
 var csjs = require('csjs-inject')
+var validator = require('solidity-validator')
 
 module.exports = displayStringInput
 
-function displayStringInput({theme: {classes: css}, type}) {
-  return bel`
+function displayStringInput({theme: {classes: css}, type, cb}) {
+  var input = bel`
     <div class=${css.stringField}>
-      <input class=${css.inputField} placeholder='abc'>
+      <input class=${css.inputField} oninput=${validate} placeholder='abc'>
     </div>`
+
+  return input
+
+  function validate (e) {
+    var msg = validator.getMessage(type, e.target.value)
+    if (msg) cb(msg)
+    else cb(null)
+  }
 }
